ac5a18d9f356a9d1a73e9a744e86a29301d2ad89
Francisco Ruiz <designfirefx@gmail.com>
2019-01-07 23:34:43 +0000

    Added preliminary routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import{
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+
 import ArtGrid  from './ArtGrid';
+
 import './App.css';
 
 class App extends Component {
@@ -7,12 +15,16 @@ class App extends Component {
   render() {
     //console.log( this.state.portfolio );
     return (
-      <div className="App">
-        <header className="App-header">
-          FXR
-        </header>
-        <ArtGrid />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">FXR</Link>
+          </header>
+          <Switch>
+            <Route exact path="/" component={ ArtGrid } />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
